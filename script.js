9c6c3a825e7f2a0a9aa58d40b7ee93821b48d68f
h24axeal <h24axeal@du.se>
2025-11-29 11:54:13 +0100

    klar med uppgift 5 och lagt till kommentarer

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-
+/* hämtar elementet med hjäp av getElementById och sparar det i variabeln checkbox */
 const checkbox =  document.getElementById("divStyle");
 console.log(checkbox);
 
+ /* hämtar elementet med hjäp av getElementsByClassName och sparar det i variabeln textfields */
 const textfields = document.getElementsByClassName("textfield");
 console.log(textfields);
 
- const button = document.querySelector("button");
+/*  hämtar elementet med hjäp av querySelector och sparar det i variabeln button */
+const button = document.querySelector("button");
 console.log(button); 
 
+/*  hämtar elementet med hjäp av getElementsByClassName och sparar det i variabeln container */
 const container = document.getElementsByClassName("container")[0];
 console.log(container); 
 
- 
+
+ /* skapar en funktion som tar emot event */
+function Userinput(e){
+    
+   const name = e.target.name;// hämtar namnet på det element som triggat eventet och sparar det i variabeln name
+   console.log(name);
+
+  if(name === "content") // kollar om namnet är lika med content
+ {
+
+   container.innerHTML = e.target.value // sätter innehållet i container och innerHTML till värdet av det element som triggat eventet
+
+ }
+}
+
+
+
+
 
 
