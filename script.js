04a56d23831d2e47291e7eb4b60a43c0f93d9b71
h24axeal <h24axeal@du.se>
2025-12-01 14:39:38 +0100

    klar med allt

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const checkbox =  document.getElementById("divStyle");
 console.log(checkbox);
 
  /* hämtar elementet med hjäp av getElementsByClassName och sparar det i variabeln textfields */
-const textfields = document.getElementsByClassName("textfield");
+const textfields = document.querySelectorAll(".textfield");
 console.log(textfields);
 
 /*  hämtar elementet med hjäp av querySelector och sparar det i variabeln button */
@@ -16,18 +16,35 @@ console.log(container);
 
 
  /* skapar en funktion som tar emot event */
-function Userinput(e){
+function userInput(e){
     
    const name = e.target.name;// hämtar namnet på det element som triggat eventet och sparar det i variabeln name
    console.log(name);
 
   if(name === "content") // kollar om namnet är lika med content
  {
-
    container.innerHTML = e.target.value // sätter innehållet i container och innerHTML till värdet av det element som triggat eventet
-
  }
+
 }
+ checkbox.addEventListener("change", function(e){
+
+      const bcColor = document.getElementById("color").value;
+      container.style.backgroundColor = bcColor;
+ });
+
+ 
+textfields.forEach(field => {
+  field.addEventListener("input", userInput);
+});
+ 
+
+  button.addEventListener("click", function(e){
+    container.remove();
+  });
+ 
+
+
 
 
 
