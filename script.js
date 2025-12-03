2d2d2ca6e4da40294a586dbbc386c889a41dda5d
h24axeal <h24axeal@du.se>
2025-12-03 16:10:39 +0100

    klart

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-document.body.style.backgroundImage = // Bakgrundsbild med moln
-  "url('https://png.pngtree.com/thumb_back/fh260/background/20250601/pngtree-seamless-pattern-of-fluffy-white-clouds-in-a-bright-blue-sky-image_17370889.jpg')";
-document.body.style.backgroundSize = "auto"; // ingen strech
-document.body.style.backgroundRepeat = "repeat"; // upprepning
-document.body.style.backgroundAttachment = "fixed"; // gör att molnen ligger stilla vid scroll
-
 /* hämtar elementet med hjäp av getElementById och sparar det i variabeln checkbox */
 const checkbox = document.getElementById("divStyle");
 console.log(checkbox);
 
-/* hämtar elementet med hjäp av getElementsByClassName och sparar det i variabeln textfields */
+ /* hämtar elementet med hjäp av getElementsByClassName och sparar det i variabeln textfields */
 const textfields = document.querySelectorAll(".textfield");
 console.log(textfields);
 
@@ -21,24 +15,44 @@ const container = document.getElementsByClassName("container")[0];
 console.log(container);
 
 /* Uppgift 5 */
-/* skapar en funktion som tar emot event */
-function userInput(e) {
-  const name = e.target.name; // hämtar namnet på det element som triggat eventet och sparar det i variabeln name
-  console.log(name); //loggar
+ /* skapar en funktion som tar emot event */
+function userInput(e){
+    
+   const name = e.target.name;// hämtar namnet på det element som triggat eventet och sparar det i variabeln name
+   console.log(name); //loggar 
 
-  if (name === "content") {
-    // kollar om namnet är lika med content
-    container.innerHTML = e.target.value; // sätter innehållet i container och innerHTML till värdet av det element som triggat eventet
-  }
+  if(name === "content") // kollar om namnet är lika med content
+ {
+   container.innerHTML = e.target.value // sätter innehållet i container och innerHTML till värdet av det element som triggat eventet
+ }
 }
 
 /* Uppgift 6 */
-checkbox.addEventListener("change", function (e) {
-  textfields.forEach((field) => {
-    field.addEventListener("input", userInput);
-  });
-});
+ checkbox.addEventListener("change", function(e){
 
-button.addEventListener("click", function (e) {
-  container.remove();
+      const bcColor = document.getElementById("color").value;
+      container.style.backgroundColor = bcColor;
+ });
+
+ 
+
+textfields.forEach(field => {
+ 
+    field.addEventListener("input", userInput);
 });
+ 
+
+
+button.addEventListener("click", function(e){
+   
+     container.remove();
+  });
+ 
+
+
+
+
+
+
+
+
