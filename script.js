bb23590d1c864ec5fd0214b5e2d6a6aba07fe1e4
h24axeal <h24axeal@du.se>
2025-12-03 15:17:53 +0100

    uppdaterat

--- script.js
+++ script.js
@@ -14,19 +14,20 @@ console.log(button);
 const container = document.getElementsByClassName("container")[0];
 console.log(container);
 
-
+/* Uppgift 5 */
  /* skapar en funktion som tar emot event */
 function userInput(e){
     
    const name = e.target.name;// hämtar namnet på det element som triggat eventet och sparar det i variabeln name
-   console.log(name);
+   console.log(name); //loggar 
 
   if(name === "content") // kollar om namnet är lika med content
  {
    container.innerHTML = e.target.value // sätter innehållet i container och innerHTML till värdet av det element som triggat eventet
  }
-
 }
+
+/* Uppgift 6 */
  checkbox.addEventListener("change", function(e){
 
       const bcColor = document.getElementById("color").value;
