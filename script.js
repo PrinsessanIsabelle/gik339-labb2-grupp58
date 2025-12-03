e04355d8a8ccd501cbb898060aa29b5b0afce50a
h24axeal <h24axeal@du.se>
2025-12-03 16:12:17 +0100

    klart

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.body.style.backgroundImage = "url( https://i.ibb.co/HD599j46/pngtree-seamless-pattern-of-fluffy-white-clouds-in-a-bright-blue-sky-image-17370889.jpg)";
+document.body.style.backgroundSize = "auto"; // ingen strech
+document.body.style.backgroundRepeat = "repeat"; // upprepning
+document.body.style.backgroundAttachment = "fixed"; // gör att molnen ligger stilla vid scroll
+
 /* hämtar elementet med hjäp av getElementById och sparar det i variabeln checkbox */
 const checkbox = document.getElementById("divStyle");
 console.log(checkbox);
@@ -45,7 +50,7 @@ textfields.forEach(field => {
 
 button.addEventListener("click", function(e){
    
-     container.remove();
+  container.remove();
   });
  
 
