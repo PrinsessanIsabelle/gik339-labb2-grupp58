6cc57ad15777208fbe2caa5a51335b0a72b9809f
Isabelle Lindberg <h24islin@du.se>
2025-12-04 14:09:20 +0100

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.body.style.backgroundImage = "url( https://i.ibb.co/HD599j46/pngtree-seamless-pattern-of-fluffy-white-clouds-in-a-bright-blue-sky-image-17370889.jpg)";
+document.body.style.backgroundImage =
+  "url( https://i.ibb.co/HD599j46/pngtree-seamless-pattern-of-fluffy-white-clouds-in-a-bright-blue-sky-image-17370889.jpg)";
 document.body.style.backgroundSize = "auto"; // ingen strech
 document.body.style.backgroundRepeat = "repeat"; // upprepning
 document.body.style.backgroundAttachment = "fixed"; // gör att molnen ligger stilla vid scroll
@@ -7,7 +8,7 @@ document.body.style.backgroundAttachment = "fixed"; // gör att molnen ligger st
 const checkbox = document.getElementById("divStyle");
 console.log(checkbox);
 
- /* hämtar elementet med hjäp av getElementsByClassName och sparar det i variabeln textfields */
+/* hämtar elementet med hjäp av getElementsByClassName och sparar det i variabeln textfields */
 const textfields = document.querySelectorAll(".textfield");
 console.log(textfields);
 
@@ -20,39 +21,34 @@ const container = document.getElementsByClassName("container")[0];
 console.log(container);
 
 /* Uppgift 5 */
- /* skapar en funktion som tar emot event */
-function userInput(e){
-    
-   const name = e.target.name;// hämtar namnet på det element som triggat eventet och sparar det i variabeln name
-   console.log(name); //loggar 
+/* skapar en funktion som tar emot event */
+function userInput(e) {
+  const name = e.target.name; // hämtar namnet på det element som triggat eventet och sparar det i variabeln name
+  console.log(name); //loggar
 
-  if(name === "content") // kollar om namnet är lika med content
- {
-   container.innerHTML = e.target.value // sätter innehållet i container och innerHTML till värdet av det element som triggat eventet
- }
+  if (name === "content") {
+    // kollar om namnet är lika med content
+    container.innerHTML = e.target.value; // sätter innehållet i container och innerHTML till värdet av det element som triggat eventet
+  }
 }
 
 /* Uppgift 6 */
- checkbox.addEventListener("change", function(e){
-
-      const bcColor = document.getElementById("color").value;
-      container.style.backgroundColor = bcColor;
- });
-
- 
-
-textfields.forEach(field => {
- 
-    field.addEventListener("input", userInput);
+checkbox.addEventListener("change", function (e) {
+  const bcColor = document.getElementById("color").value;
+  container.style.backgroundColor = bcColor;
 });
- 
 
-
-button.addEventListener("click", function(e){
-   
+button.addEventListener("click", function (e) {
   container.remove();
-  });
- 
+});
+
+textfields.forEach((field) => {
+  field.addEventListener("input", userInput);
+});
+
+button.addEventListener("click", function (e) {
+  container.remove();
+});
 
 
 
